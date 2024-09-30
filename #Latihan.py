3685f16aad6f2db126bbc91a4623faa178f6826c
Egi Julian <149486315+Egyz12@users.noreply.github.com>
2024-09-30 09:23:45 +0700

    Add files via upload

---
+++ #Latihan.py
@@ -0,0 +1,13 @@
+#Latihan
+
+# Cara 1
+nilai_mahasiawa = [90,80,78,98,78]
+print (nilai_mahasiawa)
+print (sum(nilai_mahasiawa)/len(nilai_mahasiawa))
+
+
+# Cara 2
+nilai_mahasiawa = [70,80,88,68,78]
+print (nilai_mahasiawa)
+ratarata = sum (nilai_mahasiawa)/len(nilai_mahasiawa)
+print (ratarata)
